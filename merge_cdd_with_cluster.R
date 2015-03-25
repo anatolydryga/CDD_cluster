1a8fc4bfbf224a99a769f25d0d0937d39611adfd
Anatoly Dryga <anatolydryga@gmail.com>
2015-03-25 17:02:39 -0400

    reading contig-cdd, cdd-annotation and merging them together

---
+++ merge_cdd_with_cluster.R
@@ -0,0 +1,20 @@
+#' for each cdd add custom annotation and keeping all contigs.
+#' @return datframe with 3 columns: cdd, contig, annotation
+add_annotataion_to_cdd <- function(contig_cdd, cdd_annotation) {
+    contig_cdd_annotation <- merge(contig_cdd, cdd_annotation, by=c("cdd"), all.x=TRUE)
+    contig_cdd_annotation
+}
+
+#' for each annotation count how many times it is found.
+#' @return dataframe with 2 columns: annotation and frequency(Freq)
+cdd_annotation_counts <- function(contig_cdd_annotation) {
+    counts <- as.data.frame(table(contig_cdd_annotation$annotation))
+    counts
+}
+
+#' find all cdd that does not have annotation.
+#' @return vector of cdd ids
+not_annotated_cddid <- function(contig_cdd_annotation) {
+    no_annotation <- subset(contig_cdd_annotation, is.na(annotation))
+    no_annotation$cdd
+}
--- read_cdd.R
+++ read_cdd.R
@@ -1,7 +1,15 @@
-read_contig_cdd_acc <- function() {
-    "Hello World"
+#' read file with 2 columns: contig name and cdd id.
+#' @return dataframe with contig, cdd columns. 
+read_contig_cdd <- function(filename) {
+    contig_cdd <- read.delim(filename, header=FALSE)
+    names(contig_cdd) <- c("contig", "cdd")
+    contig_cdd
 }
 
-read_cdd_clusters <- function() {
-    "Hello World"
+#' read file with 2 columns: cdd id and annotation of cdd id.
+#' @return dataframe with cdd, annotation columns.
+read_cdd_annotation <- function(filename) {
+    cdd_annotation <- read.delim(filename, header=FALSE)
+    names(cdd_annotation) <- c("cdd", "annotation")
+    cdd_annotation
 }
