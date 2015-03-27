36ee5780bd24f70bc6c88bc15965aedba4ffc2a1
Anatoly Dryga <anatolydryga@gmail.com>
2015-03-27 14:46:29 -0400

    implmentation with help of ez direct

--- cdd_description_addition.py
+++ cdd_description_addition.py
@@ -1,16 +1,37 @@
 import sys
+import tempfile
+import os
 import csv
+import subprocess
+
+def run_command(command, error_message):
+    try:
+        subprocess.check_call(command, shell=True)
+    except subprocess.CalledProcessError:
+        print error_message
 
 def get_title(cdd_accession):
     """ for a given id(e.g. pfam) pull short description"""
-    return "nobody knows"
+    output = tempfile.NamedTemporaryFile(delete=False)
+    command = ('esearch -db cdd -query "' + cdd_accession 
+        + ' [ACCN]" | efetch > ' + output.name)
+    run_command(command, "cannot get title from NCBI")
+    title = parse_output(output)
+    os.remove(output.name)
+    return title
+
+def parse_output(output_fh):
+    next(output_fh)
+    return next(output_fh)[3:].strip()
+    
 
 assert len(sys.argv) == 3
 pfam_no_description = open(sys.argv[1], "r")
 pfam_with_description = open(sys.argv[2], "w")
 
 pfam_no_description_reader = csv.reader(pfam_no_description, delimiter='\t') 
-pfam_with_description_writer = csv.writer(pfam_with_description, delimiter='\t')
+pfam_with_description_writer = csv.writer(
+    pfam_with_description, delimiter='\t', quoting=csv.QUOTE_NONE)
 
 for row in pfam_no_description_reader:
     title = get_title(row[0])
